e621140c683ec49a5611f0b519a0ce6f504a2d81
Alessandro Cortese <ale.cortese99@gmail.com>
2024-07-26 16:06:10 +0200

    Firsto Commit

---
+++ Train_Simulator/src/main/java/it/uniroma2/pmcsn/trainsimulator/Train_Simulator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package it.uniroma2.pmcsn.trainsimulator;
+
+/**
+ *
+ * @author alessandro
+ */
+public class Train_Simulator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
